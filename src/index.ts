f743e3f04875e23bc6e5831cbf934b3005578d83
mkobar <mkobar@rkosecurity.com>
2022-03-22 14:47:06 -0400

    fixed ports for all 3 FEs

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,8 @@ createConnection().then(async () => {
     app.use(express.json());
     app.use(cors({
         credentials: true,
-        origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000']
+        //origin: ['http://localhost:3000', 'http://localhost:4000', 'http://localhost:5000']
+        origin: ['http://localhost:4200', 'http://localhost:4300', 'http://localhost:5000']
     }));
 
     routes(app);
